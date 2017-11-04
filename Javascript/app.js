15958fac6fc838aa14807c05b40765736740ba3d
matlittle <matlittle727@gmail.com>
2017-11-04 17:38:43 -0500

    Added test functions for pass and fail

--- Javascript/app.js
+++ Javascript/app.js
@@ -2,25 +2,25 @@ $(document).ready(function()
 {
 
 	$(".secondPageLayout").fadeOut();
-    $(".thirdPageLayout").fadeOut();
-    $(".fourthPageLayout").fadeOut();
-    
+	$(".thirdPageLayout").fadeOut();
+	$(".fourthPageLayout").fadeOut();
+	
 
 	 
-    $("#readyUp").click(function(){                     // this will fade out the initial start page
-        $(".firstPageLayout").fadeOut();                // fades out first page 
-        $(".secondPageLayout").fadeIn();            // fades in second page 
-    });
+	$("#readyUp").click(function(){                     // this will fade out the initial start page
+		$(".firstPageLayout").fadeOut();                // fades out first page 
+		$(".secondPageLayout").fadeIn();            // fades in second page 
+	});
 
 
-    $("").click(function()
-    {
-        $("").fadeOut();
-        // fades in the question page on button click
-        $(".").fadeOut();
-        $("").fadeIn();
+	$("").click(function()
+	{
+		$("").fadeOut();
+		// fades in the question page on button click
+		$(".").fadeOut();
+		$("").fadeIn();
 
-    });
+	});
 });
 
 
@@ -29,123 +29,130 @@ $(document).ready(function()
 
 /*Input Evaluation Function*/
 
-testString = "var myTest = 1;" +
-            "while(myTest < 10){ myTest += 1 }" +
-            "postMessage(myTest);";
-
-testQuestion = {
-            question: "Actual question",
-            asked: "false",
-            function: {
-                name: "myFunc,
-                args: ["num1", "num2"],
-            },
-            tests: [{
-                params: ["1", "2"],
-                passVal: 3
-            },
-            {
-                params: ["3", "4"],
-                passVal: 7
-            }]
-}
-
-
-checkUserCode(testString, testQuestion); 
-
-/**/
 function checkUserCode(str, question, passFunc, failFunc) {
 
-    if (typeof (Worker) === undefined) {        // check if browser supports web workers 
-        alert('No webworker supported');
-        return false;
-    }
+	if (typeof (Worker) === undefined) {        // check if browser supports web workers 
+		alert('No webworker supported');
+		return false;
+	}
 
-    var myWorker = null,
-        URL = window.URL || (window.webkitURL);      // initialize variables
+	var myWorker = null,
+		URL = window.URL || (window.webkitURL);      // initialize variables
 
-    window.URL = URL;
+	window.URL = URL;
 
-    var currTest = 0;
+	var currTest = 0;
 
-    question.tests.forEach(function(test) {
-        var functionString = buildFunctionString(test);
+	question.tests.forEach(function(test) {
+		var functionString = buildFunctionString(test);
 
-        /* (Stack Overflow comment) we are creating a new javascript file using blob. We just use the string passed to the function, and assign it to the Blob(content,type). */
-        var workerData = new Blob([functionString], { type: "text/javascript" });
+		/* (Stack Overflow comment) we are creating a new javascript file using blob. We just use the string passed to the function, and assign it to the Blob(content,type). */
+		var workerData = new Blob([functionString], { type: "text/javascript" });
 
-        initWorker(workerData, test);
-    });
+		initWorker(workerData, test);
+	});
 
 
-    function initWorker(data, test) {
-        /* (Stack Overflow comment) create the new web worker as we dont have an external file, we have to create a url for our blob using createObjectURL. link will look like blob:d3958f5c-0777-0845-9dcf-2cb28783acaf */
-        myWorker = new Worker(window.URL.createObjectURL(data));
+	function initWorker(data, test) {
+		/* (Stack Overflow comment) create the new web worker as we dont have an external file, we have to create a url for our blob using createObjectURL. link will look like blob:d3958f5c-0777-0845-9dcf-2cb28783acaf */
+		myWorker = new Worker(window.URL.createObjectURL(data));
 
-        /* listen for messages sent back by the worker */
-        myWorker.onmessage = function (e) {
-            clearTimeout(timeoutError);     // clear the error timeout so it doesn't fire
-            stopWorker(myWorker);     // delete the actual worker once we have the return
-            handleWorkerReturn(e, test);      // pass return to handler function
-            
-        };
+		/* listen for messages sent back by the worker */
+		myWorker.onmessage = function (e) {
+			clearTimeout(timeoutError);     // clear the error timeout so it doesn't fire
+			stopWorker(myWorker);     // delete the actual worker once we have the return
+			handleWorkerReturn(e, test);      // pass return to handler function
+			
+		};
 
-        var timeoutError = setTimeout(function() {
-            /* if the worker is running for longer than 5 seconds, throw timeout */
-            console.log("Timeout error thrown");
-            stopWorker(myWorker);
-            failFunc();
-        }, 5 * 1000);
-    }
+		var timeoutError = setTimeout(function() {
+			/* if the worker is running for longer than 5 seconds, throw timeout */
+			console.log("Timeout error thrown");
+			stopWorker(myWorker);
+			failFunc();
+		}, 5 * 1000);
+	}
 
-    function stopWorker(w){
-        /* stop and delete the worker when it's done */
-        w.terminate();
-        delete w;
-    }
+	function stopWorker(w){
+		/* stop and delete the worker when it's done */
+		w.terminate();
+		delete w;
+	}
 
-    function handleWorkerReturn(e, test) {
-        console.log(e);
-        console.log(e.data);
+	function handleWorkerReturn(e, test) {
+		console.log(e);
+		console.log(e.data);
 
-        /* if the return matches the expected passing value, 
-            increment the current test counter
-            if the counter equals the number of tests, then run pass function */
-        if(e.data = test.return) {
-            currTest += 1;
-            if(currTest === question.tests.length) {
-                passFunc();
-            }
-        } else {
-            failFunc();
-        }
-    }
+		/* if the return matches the expected passing value, 
+			increment the current test counter
+			if the counter equals the number of tests, then run pass function */
+		if(e.data = test.return) {
+			currTest += 1;
+			if(currTest === question.tests.length) {
+				passFunc();
+			}
+		} else {
+			failFunc();
+		}
+	}
 
-    function buildFunctionString(test) {
-        var f = question.function;
-        var args = buildArgList(f.args);
-        var params = buildArgList(test.params);
+	function buildFunctionString(test) {
+		var f = question.function;
+		var args = buildArgList(f.args);
+		var params = buildArgList(test.params);
 
-        var fullStr = `function ${f.name}(${args}) { `;
-        fullStr += `${str} }`;
+		var fullStr = `function ${f.name}(${args}) { `;
+		fullStr += `${str} }`;
 
-        fullStr += `postMessage( ${f.name}(${params}) );`;
+		fullStr += `postMessage( ${f.name}(${params}) );`;
 
-        return fullStr;
-    }
+		return fullStr;
+	}
 
-    function buildArgList(a) {
-        var list = "";
+	function buildArgList(a) {
+		var list = "";
 
-        a.forEach(function(arg){
-            if(list.length === 0) {
-                list += arg;
-            } else {
-                list += `, ${arg}`;
-            }
-        });
+		a.forEach(function(arg){
+			if(list.length === 0) {
+				list += arg;
+			} else {
+				list += `, ${arg}`;
+			}
+		});
 
-        return list;
-    }
+		return list;
+	}
 
+}
+
+/* TEST VALUES */
+testString = "var myTest = 1;" +
+			"while(myTest < 10){ myTest += 1 }" +
+			"postMessage(myTest);";
+
+testQuestion = {
+	question: "Actual question",
+	asked: "false",
+	function: {
+		name: "myFunc,
+		args: ["num1", "num2"],
+	},
+	tests: [{
+		params: ["1", "2"],
+		passVal: 3
+	},
+	{
+		params: ["3", "4"],
+		passVal: 7
+	}]
+}
+
+checkUserCode(testString, testQuestion, passedTests, failedTests); 
+
+function passedTests() {
+	console.log("passed");
+}
+
+function failedTests() {
+	console.log("failed");
 }
