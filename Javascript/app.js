fbd1dd7212b396d9d2b0b1913d88a326e93bed1e
Will <wbattles87@yahoo.com>
2017-11-12 09:11:55 -0600

    updated index, css, and js files

--- Javascript/app.js
+++ Javascript/app.js
@@ -115,6 +115,63 @@ $("#login-btn").on("click", function(event){
 
 });
 
+// New Will Stuff
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Closes the sidebar menu
+  $("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+
+  // Opens the sidebar menu
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+
+
+
+
+  //#to-top button appears after scrolling
+  var fixed = false;
+  $(document).scroll(function() {
+    if ($(this).scrollTop() > 250) {
+      if (!fixed) {
+        fixed = true;
+        $('#to-top').show("slow", function() {
+          $('#to-top').css({
+            position: 'fixed',
+            display: 'block'
+          });
+        });
+      }
+    } else {
+      if (fixed) {
+        fixed = false;
+        $('#to-top').hide("slow", function() {
+          $('#to-top').css({
+            display: 'none'
+          });
+        });
+      }
+    }
+  });
+
+})(jQuery);
+
+
+// End of New Will Stuff
+
+
+
+
+
+
+
+
+
 //add sign out event
 $("#sign-out").on("click", function(event){
 	event.preventDefault();
